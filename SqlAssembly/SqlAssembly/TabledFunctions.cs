32bf3e3e51da0f47f7786a90c88f03311123673e
Ivan Kurochkin <ivan.kurochkin@gmail.com>
2015-07-19 18:17:07 +0300

    Add projects

---
+++ SqlAssembly/SqlAssembly/TabledFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+
+namespace SqlAssembly
+{
+    public class TabledFunctions
+    {
+        //[Microsoft.SqlServer.Server.SqlFunction]
+        //public static SqlString SqlFunction1()
+        //{
+        //    // Put your code here
+        //    return new SqlString("My fucking foo 2");
+        //}
+
+        [Microsoft.SqlServer.Server.SqlFunction(FillRowMethodName = "FillRow")]
+        public static IEnumerable SqlFunction2()
+        {
+            // Put your code here
+            return new [] {new Item {Id = 1, Name = "adsfasdf"}, new Item {Id = 2, Name = "adsfasdddddf"}};
+        }
+
+        public static void FillRow(Object obj, out SqlInt32 id, out SqlString name)
+        {
+            Item eventLogEntry = (Item)obj;
+            id = new SqlInt32(eventLogEntry.Id);
+            name = new SqlString(eventLogEntry.Name);
+        }
+    }
+
+
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
